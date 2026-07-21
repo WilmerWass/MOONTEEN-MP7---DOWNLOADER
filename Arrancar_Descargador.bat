9c9c34b90df380c3d16a97164d9ad1089879b624
Wilmer_Wass <81403760+WilmerWass@users.noreply.github.com>
2026-07-20 23:13:18 -0500

    Delete Arrancar_Descargador.bat
    nuevo ejecutor On_WASSLink_MPDL

--- Arrancar_Descargador.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Lanzador de Gestor de Descargas MUNDO
-:: Obtiene la ruta exacta de la carpeta donde esta guardado este archivo .bat
-cd /d "%~dp0"
-:: Lanza PowerShell de forma transparente, salta politicas y ejecuta el script .ps1
-powershell -NoProfile -ExecutionPolicy Bypass -File "descargar_musica.ps1"
-pause
